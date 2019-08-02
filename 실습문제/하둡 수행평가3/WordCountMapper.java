185be4f85cca843c6c97d41d653df3af61c4eddf
hsyun89 <hsyun89@naver.com>
2019-08-02 17:21:30 +0900

    .

---
+++ 실습문제/하둡 수행평가3/WordCountMapper.java
@@ -0,0 +1,26 @@
+package exercise1;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends
+Mapper<LongWritable, Text, Text, IntWritable> {
+private final static IntWritable one = new IntWritable(1);
+private Text word = new Text();   
+public void map(LongWritable key, Text value, Context context)
+			 throws IOException, InterruptedException {
+StringTokenizer itr = new StringTokenizer(value.toString());
+while (itr.hasMoreTokens()) {
+	String aa=itr.nextToken();
+	if(aa.length()>=3 && aa.length()<=5) {
+		word.set(aa);
+		context.write(word, one);
+	}
+}
+}
+}
